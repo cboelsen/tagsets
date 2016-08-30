bcbf63d31299f418688e1ccb8ca2a272a1172faa
Gerard Thornley <gerard@sarahandgerard.org.uk>
2016-08-30 12:09:24 +0100

    Initial check-in - start adding system tests.

---
+++ systemtest/test_features.py
@@ -0,0 +1,35 @@
+import os
+import pytest
+
+import tagsets.cli
+
+from tagsets.test.testsupport import PathGenerator, working_directory
+
+# Path to the test files / directories
+TEST_DATA_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "feature_test_data")
+
+pg = PathGenerator(TEST_DATA_PATH)
+
+# Tests
+
+def test_no_args():
+    with pytest.raises(SystemExit):
+        tagsets.cli.main([])
+
+def test_list_tags(capsys):
+    testdir = pg.getsubgenerator("list_tags")
+
+    with working_directory(testdir.getroot()):
+        tagsets.cli.main(['-c', testdir.getpath('config.yaml'),
+                          '--list-tags'])
+
+    out,err = capsys.readouterr()
+    print("\n\n=====")
+    print(out)
+    print("=====\n\n")
+
+    # Output should have a heading
+    assert("tags\n====" in out)
+
+    # Output should contain found tag (tag 'TAG-1' at line 1 in file1 in subdir1)
+    assert( (testdir.getpath('subdir1/file1') + ':1 : TAG-1') in out)
--- tagsets/test/testsupport.py
+++ tagsets/test/testsupport.py
@@ -1,5 +1,7 @@
 import os
 
+from contextlib import contextmanager
+
 class PathGenerator:
     def __init__(self, basedir):
         self.rootpath=basedir
@@ -13,3 +15,11 @@ class PathGenerator:
     def getroot(self):
         return self.rootpath
 
+@contextmanager
+def working_directory(newdir):
+    prevdir = os.getcwd()
+    os.chdir(os.path.expanduser(newdir))
+    try:
+        yield
+    finally:
+        os.chdir(prevdir)
